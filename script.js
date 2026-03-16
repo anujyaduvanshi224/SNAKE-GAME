b8cc446863dce390a88818263cd955e1af87e87f
Anuj Yaduvanshi <216862195+anujyaduvanshi224@users.noreply.github.com>
2026-03-16 21:04:59 +0530

    implemented the snake direction control logic

---
+++ script.js
@@ -0,0 +1,68 @@
+const board = document.querySelector(".board");
+const blockHeight = 30;
+const blockWidth = 30;
+
+const cols = Math.floor(board.clientWidth / blockWidth);
+const rows = Math.floor(board.clientHeight / blockHeight);
+
+const blocks = [];
+const snake = [{x:1,y:3}];
+let direction = "left";
+
+// for (let i = 0; i < rows * cols; i++){
+//     const block = document.createElement("div");
+//     block.classList.add("block");
+//     board.appendChild(block);
+// }
+
+for (let row = 0; row < rows; row++){
+    for (let col = 0; col < cols; col++){
+        const block = document.createElement("div");
+        block.classList.add("block");
+        board.appendChild(block);
+        blocks[`${row}-${col}`] = block;
+    }
+}
+
+function render(){
+    // Implementation for rendering the game state
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.add("fill");
+    })
+}
+
+setInterval(() => {
+    let head = null
+
+    if (direction === "left"){
+        head = {x: snake[0].x, y: snake[0].y - 1};
+    }else if (direction === "right"){
+        head = {x: snake[0].x, y: snake[0].y + 1};
+    }else if (direction === "up"){
+        head = {x: snake[0].x - 1, y: snake[0].y};
+    }else if (direction === "down"){
+        head = {x: snake[0].x + 1, y: snake[0].y};  
+    }
+
+
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.remove("fill");
+    })
+
+    snake.unshift(head);
+    snake.pop();
+    render();
+},200); 
+
+addEventListener("keydown", (e) => {
+    if(e.key === "ArrowLeft"){
+        direction = "left";
+    }else if(e.key === "ArrowRight"){
+        direction = "right";
+    }else if(e.key === "ArrowUp"){
+        direction = "up";
+    }else if(e.key === "ArrowDown"){
+        direction = "down";
+    }
+})
+
