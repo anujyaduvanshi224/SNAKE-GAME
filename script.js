791ee697f802c23b9c5386d007bc7af6bfa99222
Anuj Yaduvanshi <216862195+anujyaduvanshi224@users.noreply.github.com>
2026-03-16 22:22:04 +0530

    add the logic for the creation of start game and restart game modal

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const board = document.querySelector(".board");
 const blockHeight = 30;
 const blockWidth = 30;
+const startButton = document.querySelector(".btn-start");
+const modal = document.querySelector(".modal");
+const startGameModal = document.querySelector(".start-game");
+const gameOverModal = document.querySelector(".game-over");
+const reStartButton = document.querySelector(".btn-restart");
+ 
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
 
 const blocks = [];
-const snake = [{x:1,y:3}];
+let snake = [{x:1,y:3}];
 let food = {x:Math.floor(Math.random() * rows), y:Math.floor(Math.random() * cols)};
 let direction = "left";
 let intervalId = null;
@@ -44,7 +50,12 @@ function render(){
 
     if(head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols){
         clearInterval(intervalId);
-        alert("Game Over");
+        
+        modal.style.display = "flex";
+        startGameModal.style.display = "none";
+        gameOverModal.style.display = "flex";
+         
+        return;
     }
 
     if(head.x === food.x && head.y === food.y){
@@ -66,10 +77,28 @@ function render(){
     })
 }
 
-intervalId = setInterval(() => {
-    
-    render();
-},300); 
+
+startButton.addEventListener("click", () => {
+    modal.style.display = "none";
+    intervalId = setInterval(() => {
+        render();
+    },300);
+});
+
+reStartButton.addEventListener("click", restartGame )
+
+function restartGame(){
+    blocks[`${food.x}-${food.y}`].classList.remove("food");
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.remove("fill");
+    })
+
+    modal.style.display = "none";
+    direction = "down";
+    snake =[{x:1,y:3}];
+    food = {x:Math.floor(Math.random() * rows), y:Math.floor(Math.random() * cols)};
+    intervalId = setInterval(() => { render() },300);
+}
 
 addEventListener("keydown", (e) => {
     if(e.key === "ArrowLeft"){
